934e25696d6e8299376887f7d9439547882c790e
vipin mishra <vipin2003mishra@gmail.com>
2024-06-30 23:46:26 +0530

    code pushingg

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,34 +1,37 @@
-
-const mongoose = require('mongoose');
-const faculty = require('../Models/FacultySchema');
- const Database = async () => {
+const mongoose = require("mongoose");
+const faculty = require("../Models/FacultySchema");
+const bcrypt = require("bcrypt");
+const Database = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Database Connection successfully');
+    console.log("Database Connection successfully");
 
-      const adminData = {
+    const adminData = {
       first_name: "Vipin",
       last_name: "Mishra",
       email: "vipin2003mishra@gmail.com",
-      password: "Admin@123",
+      password: bcrypt.hashSync("Admin@123", 10),
       role: "ADMIN",
       status: "ACTIVE",
       country: "India",
       state: "uttar pradesh",
       city: "pratapgarh",
       dialing_code: +91,
-      phone: 6306324384,      
+      phone: 6306324384,
       // qr: adminData._id,
     };
-    const existingAdmin = await faculty.findOne({$and:[{ email: adminData.email },{ role: "ADMIN" }]});
-    if (!existingAdmin) {
-      faculty.create(adminData)
-     }
-    else {
-      console.log(`Default admin is created`)
+    const existingAdmin = await faculty.findOne({
+      $and: [{ email: adminData.email }, { role: "ADMIN" }],
+    });
+    if (existingAdmin) {
+      console.log("Default Admin is already created");
     }
-  }catch (error) {
+    if (!existingAdmin) {
+      faculty.create(adminData);
+      console.log(`Default admin is created`, adminData);
+    }
+  } catch (error) {
     console.error(error);
   }
 };
-module.exports=Database;
+module.exports = Database;
